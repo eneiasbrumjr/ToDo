3fdc8a0bc01f307ecce9e1ed261b031d321f82c6
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-10-23 13:35:52 -0300

    Added feat mark as done

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Task
 
 
@@ -7,3 +7,9 @@ def addTask(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
     return redirect("home")
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
