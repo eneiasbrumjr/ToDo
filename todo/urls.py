8ff3843be40268950783d4efdad7df8fa90a69a8
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-10-24 11:39:46 -0300

    Added edit task feature

--- todo/urls.py
+++ todo/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    #Add task
     path("addTask/", views.addTask, name="addTask"),
+    #Mark as done
     path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
-    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone')
+    #Mark as Undone
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    #Edit Feature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task')
 ]
 
