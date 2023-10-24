3480134815694a52e9e6c75dd68d7df4322b3215
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-10-24 11:45:07 -0300

    Added delete task feature

--- todo/urls.py
+++ todo/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     #Mark as Undone
     path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
     #Edit Feature
-    path('edit_task/<int:pk>/', views.edit_task, name='edit_task')
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    #Delete Feature
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task')
 ]
 
